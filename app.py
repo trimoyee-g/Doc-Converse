2e2acdc26834f96e7d42cac1e748f8d385a2d5ff
trimoyee-g <trimoyeeg@gmail.com>
2024-02-09 15:52:11 +0530

    added website interaction facility

--- app.py
+++ app.py
@@ -19,6 +19,16 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+# from langchain.embeddings import OpenAIEmbeddings, ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
 def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
@@ -70,6 +80,102 @@ def handle_userinput(user_question):
             st.write(bot_template.replace(
                 "{{MSG}}", message.content), unsafe_allow_html=True)
 
+def get_vectorstore_from_url(url):
+    # get the text in document form
+    loader = WebBaseLoader(url)
+    document = loader.load()
+    
+    # split the document into chunks
+    text_splitter = RecursiveCharacterTextSplitter()
+    document_chunks = text_splitter.split_documents(document)
+    
+    # create a vectorstore from the chunks
+    vector_store = Chroma.from_documents(document_chunks, OpenAIEmbeddings())
+
+    return vector_store
+
+def get_context_retriever_chain(vector_store):
+    llm = ChatOpenAI()
+    
+    retriever = vector_store.as_retriever()
+    
+    prompt = ChatPromptTemplate.from_messages([
+      MessagesPlaceholder(variable_name="chat_history"),
+      ("user", "{input}"),
+      ("user", "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
+    ])
+    
+    retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+    
+    return retriever_chain
+    
+def get_conversational_rag_chain(retriever_chain): 
+    
+    llm = ChatOpenAI()
+    
+    prompt = ChatPromptTemplate.from_messages([
+      ("system", "Answer the user's questions based on the below context:\n\n{context}"),
+      MessagesPlaceholder(variable_name="chat_history"),
+      ("user", "{input}"),
+    ])
+    
+    stuff_documents_chain = create_stuff_documents_chain(llm,prompt)
+    
+    return create_retrieval_chain(retriever_chain, stuff_documents_chain)
+
+def get_response(user_input):
+    vector_store = st.session_state.vector_store
+    retriever_chain = get_context_retriever_chain(vector_store)
+    conversation_rag_chain = get_conversational_rag_chain(retriever_chain)
+    
+    response = conversation_rag_chain.invoke({
+        "chat_history": st.session_state.chat_history,
+        "input": user_input
+    })
+    
+    return response['answer']
+
+
+def website_page():
+    st.header("Chat with Websites")
+    st.write("Empower conversations with websites. Engage in interactive discussions directly within websites using AI-driven chat functionalities!")
+    
+# sidebar
+    with st.sidebar:
+       if st.button("<"):
+           st.session_state.page = "Landing Page"
+       # st.header("Settings")
+       website_url = st.text_input("Website URL")
+
+    if website_url is None or website_url == "":
+      st.info("Please enter a website URL")
+
+    else:
+       # session state
+       if "chat_history" not in st.session_state:
+           st.session_state.chat_history = [
+            AIMessage(content="Ask a question about the website:"),
+        ]
+    if "vector_store" not in st.session_state:
+        st.session_state.vector_store = get_vectorstore_from_url(website_url)    
+
+    # user input
+    user_query = st.chat_input("Type your message here...")
+    if user_query is not None and user_query != "":
+        response = get_response(user_query)
+        st.session_state.chat_history.append(HumanMessage(content=user_query))
+        st.session_state.chat_history.append(AIMessage(content=response))
+        
+       
+
+    # conversation
+    for message in st.session_state.chat_history:
+        if isinstance(message, AIMessage):
+            with st.chat_message("AI"):
+                st.write(message.content)
+        elif isinstance(message, HumanMessage):
+            with st.chat_message("Human"):
+                st.write(message.content)
 
 def pdf_page():
     if "conversation" not in st.session_state:
@@ -187,7 +293,7 @@ def perform_ocr(image):
 def landing_page():
     st.title("Welcome to DocConverse")
     st.markdown("")
-    st.subheader("Seamlessly Interact with PDFs, CSVs, and Handwritten Notes!")
+    st.subheader("Seamlessly Interact with PDFs, CSVs, Websites, and Handwritten Notes!")
     st.markdown("")
     st.markdown("")
     st.write("Hello human! How may I help you today?")
@@ -196,6 +302,8 @@ def landing_page():
         st.session_state.page = "PDF Page"
     elif st.button("Chat with multiple CSV"):
         st.session_state.page = "CSV Page"
+    elif st.button("Chat with Websites"):
+        st.session_state.page = "Website Page"
     elif st.button("Convert Handwritten Note to Text File"):
         st.session_state.page = "HANDWRITTEN Page"
 def main():
@@ -212,6 +320,8 @@ def main():
         landing_page()
     elif st.session_state.page == "PDF Page":
         pdf_page()
+    elif st.session_state.page == "Website Page":
+        website_page()
     elif st.session_state.page == "HANDWRITTEN Page":
         handwritten_to_text()
     elif st.session_state.page == "CSV Page":
